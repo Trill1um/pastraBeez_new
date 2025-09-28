cfbf2a012356a7d2b2f02dca9fbff0507ffa2289
troy <menardtroyf@gmail.com>
2025-09-28 13:11:38 +0800

    chang ed Port Settingsw

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
   connectDB();
 });
