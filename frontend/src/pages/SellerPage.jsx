3b554293fa6ccd7939a8d576be9a080e49fa0c4b
troy <menardtroyf@gmail.com>
2025-09-29 06:41:04 +0800

    additional checked

--- frontend/src/pages/SellerPage.jsx
+++ frontend/src/pages/SellerPage.jsx
@@ -32,7 +32,7 @@ const SellerProducts = ({ user }) => {
     Other: <OtherIcon className="text-amber-600 stroke-current w-5 h-5" />,
   };
 
-  const { sellerProducts, isLoading, error } = useSellerProducts(user._id);
+  const { sellerProducts, isLoading, error } = useSellerProducts(user?._id);
   const { deleteProductAsync, isDeleting, isCreating } = useProcessedProducts();
 
   // Update local products when filtered products change
