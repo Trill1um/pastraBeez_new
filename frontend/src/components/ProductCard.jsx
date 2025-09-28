1cb55bfcbccaf3518bf8d08c16833bc806c7d523
troy <menardtroyf@gmail.com>
2025-09-28 21:44:54 +0800

    fixed buy

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -36,7 +36,7 @@ const ProductCard = ({ product, isPreview = false }) => {
 
   //open messenger link in new tab
   const onBuy = () => {
-    window.open(product.sellerId.facebookLink, "_blank");
+    window.open("https://m.me/" + product.sellerId.facebookLink, "_blank");
   };
 
   return (
@@ -110,7 +110,7 @@ const ProductCard = ({ product, isPreview = false }) => {
             {/* Product Name */}
             <h3 className="max-w-[320px] item-name truncate">{product.name}</h3>
             <div className="flex items-center justify-between h-[2rem]">
-              <p className="bee-price max-w-[180px] truncate item-price">
+              <p className="max-w-[180px] truncate item-price">
                 P{" "}
                 {Number(product.price).toLocaleString("en-US", {
                   maximumFractionDigits: 2,
