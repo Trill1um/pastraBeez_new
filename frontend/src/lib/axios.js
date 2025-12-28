32a91c8d9b542316238b61a2d6029008ac9ac4c0
Troy <97826350+Trill1um@users.noreply.github.com>
2025-12-28 12:21:10 +0800

    Reduce axios timeout from 120s to 2s

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -7,7 +7,7 @@ const baseURL =
 const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
-  timeout: 120000,
+  timeout: 2000,
   headers: {
     "Content-Type": "application/json",
   },
