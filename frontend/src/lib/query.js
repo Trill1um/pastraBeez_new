d6432d743f0e20ece904623fc859f3980cca2e60
Troy <97826350+Trill1um@users.noreply.github.com>
2025-12-28 12:48:41 +0800

    Correct alert for response time in query.js
    Fix alert message formatting for response time.

--- frontend/src/lib/query.js
+++ frontend/src/lib/query.js
@@ -21,6 +21,7 @@ const createProduct = async (productData) => {
   response.time=Date.now()-now;
   toast.success(response.data.message);
   toast.success(`Time: ${response.time}ms`, {duration: 10000});
+  alert(`$Time: {response.time/60}s`);
   console.log(response);
   return response.data;
 };
